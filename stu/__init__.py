043b3cf6a5af48eb0252f40feface4ed044af90b
codeHauler-1 <50894822+codeHauler-1@users.noreply.github.com>
2021-09-14 11:04:09 +0800

    Update __init__.py

--- stu/__init__.py
+++ stu/__init__.py
@@ -4,6 +4,6 @@
 #   Author        : 电子羊
 #   E-mail        : 732656715@qq.com
 #   Created date  : 2021/4/2
-#   Description   : 深圳文达智通技术有限公司  @版权所有
+#   Description   : 
 # ================================================
 from .struct import *
